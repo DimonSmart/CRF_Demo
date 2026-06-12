696624fd2655f6b3a14d457def3ae97333cee72c
Dmitry Dorogoy <dorogoj@live.ru>
2026-06-12 14:52:49 +0200

    Run bat fix cutted params line

--- RunCrfClassifier.bat
+++ RunCrfClassifier.bat
@@ -7,11 +7,23 @@ set "DEFAULT_TEXT=CITALOPRAM NORMON 20MG 28 COMPRIMIDOS EFG"
 
 cd /d "%ROOT%"
 
-if "%~1"=="" (
-  set "TEXT=%DEFAULT_TEXT%"
+if "%~1"=="" goto use_default_text
+
+set "TEXT="
+:collect_args
+if "%~1"=="" goto run_classifier
+if defined TEXT (
+  set "TEXT=%TEXT% %~1"
 ) else (
-  set "TEXT=%*"
+  set "TEXT=%~1"
 )
+shift
+goto collect_args
+
+:use_default_text
+set "TEXT=%DEFAULT_TEXT%"
+
+:run_classifier
 
 dotnet run --project "%ROOT%src\CrfDemo" -- parse ^
   --model "%MODEL%" ^
